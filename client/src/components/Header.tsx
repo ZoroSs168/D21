9490e6be4c1ef845721abba9052c688bf3e72b74
unknown <bimltv10@gmail.com>
2024-08-02 10:42:42 +0700

    chinh header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -36,71 +36,34 @@ const Header = () => {
   );
 
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="#">
-        Navbar
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">
-              Home
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Features
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Pricing
-            </a>
-          </li>
-          <li className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle"
-              href="#"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Dropdown link
-            </a>
-            <ul className="dropdown-menu">
-              <li>
-                <a className="dropdown-item" href="#">
-                  Action
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Another action
-                </a>
-              </li>
-              <li>
-                <a className="dropdown-item" href="#">
-                  Something else here
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+    <Wrapper
+    sx={{ padding: "0 50px" }}
+    direction={"row"}
+    justifyContent={"space-between"}
+    alignItems={"center"}
+  >
+    <img src="/logo.svg" alt="logo" />
+    <Stack direction={"row"} gap={"75px"}>
+      {/* menu */}
+      {menus.map((menu, index) => (
+        <Link to={menu.link} key={index}>
+          <Typography fontWeight={"500"}>{menu.label}</Typography>
+        </Link>
+      ))}
+    </Stack>
+    <Stack gap={"45px"} direction={"row"}>
+      <Link to={"/login"}>
+        <img src="/user.svg" alt="user" />
+      </Link>
+      <SearchIcon />
+      <FavoriteBorderIcon />
+      <Link to={"/cart"}>
+        <Badge badgeContent={cartQuantity} color="secondary">
+          <img src="/cart.svg" alt="cart" />
+        </Badge>
+      </Link>
+    </Stack>
+  </Wrapper>
   
   );
 };
